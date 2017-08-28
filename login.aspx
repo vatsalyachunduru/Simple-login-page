7192cda02a37f201947e08d7159193784a00c5cc
vatsalyachunduru <31423086+vatsalyachunduru@users.noreply.github.com>
2017-08-28 19:09:21 -0400

    Add files via upload

---
+++ login.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="WebApplication10.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                
+                <h1 style="background-color:pink">Login</h1>
+                <img src="Images/Lock.PNG" />
+                
+                <table cellspacing="0" border="5" style="border-color:blue"><tr><td>
+                    <table  cellPadding="20" cellspacing="0">
+                        
+                        <tr><td>user id</td><td><asp:TextBox ID="txtUID" runat="server"></asp:TextBox></td></tr>
+                        <tr><td>Password</td><td><asp:Textbox ID="txtPWD" runat="server"></asp:Textbox></td></tr>
+                        <tr><td colspan="2" align="center"><asp:Button ID="btnLogin" text="Login" runat="server" OnClick="btnLogin_Click" /></td></tr>
+                        <tr><td colspan="2"><asp:Label ID="lblMessage" runat="server"></asp:Label></td></tr>
+                        
+                    </table>
+                </td></tr></table>
+                <br />
+                <hr />
+                &copy; www.LearnTopics.com,2017
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+
+namespace WebApplication10
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            //connection
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=VATSALYA-PC\\SQLEXPRESS;Initial Catalog=ITSYSTEMS;Integrated Security=true";
+            //command
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select count(*) from users where loginid='" + txtUID.Text + "' and loginpwd='" + txtPWD.Text + "';";
+            cmd.Connection = conn;
+            //open connection
+            conn.Open();
+            //Execute command and get single value
+            var result = cmd.ExecuteScalar();
+            if(Convert.ToInt32(result)==0)
+                {
+                lblMessage.Text = "Invalid Credentials";
+                lblMessage.ForeColor=System.Drawing.Color.Red;
+                lblMessage.Visible = true;
+            }
+            else
+            {
+                Session["isvalid"] = "Yes";
+                Response.Redirect("Home.aspx");
+
+            }
+
+            //Close connection
+            conn.Close();
+
+
+
+        }
+    }
+}
